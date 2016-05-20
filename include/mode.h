e5d2e8f6b3eea20f1f97bafe9d0caf4920416bb0
Gary Ellingson <gellings13@gmail.com>
2016-05-20 15:13:11 -0600

    fixed the build error from the enum declerations

--- include/mode.h
+++ include/mode.h
@@ -1,28 +1,28 @@
-enum controlMode_t{
+typedef enum {
   ALT_MODE,
   ATTITUDE_MODE,
   RATE_MODE,
   PASSTHROUGH
-};
+} controlMode_t;
 controlMode_t onboard_control_mode;
 controlMode_t rc_control_mode;
 controlMode_t composite_control_mode;
 
-enum armedState_t{
+typedef enum {
   ARMED,
   DISARMED
-};
+} armedState_t;
 armedState_t armed_state;
 
-enum overriedMode_t{
+typedef enum {
   OFFBOARD,
   OFFBOARD_MIN_THROTTLE,
   MANUAL_RC
-};
+} overriedMode_t;
 overriedMode_t override_mode;
 
-enum errorState_t{
+typedef enum {
   INVALID_CONTROL_MODE,
   INVALID_ARMED_STATE,
-};
+} errorState_t;
 errorState_t error_state;
