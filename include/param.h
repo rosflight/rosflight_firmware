59f2cd683dc9badf6af5f9660f0660d5b3a7a201
Daniel Koch <daniel.koch@byu.edu>
2016-05-20 18:03:04 -0600

    Changed param id type to an enum

--- include/param.h
+++ include/param.h
@@ -5,70 +5,80 @@
 
 #include "mavlink.h"
 
-#define PARAMS_COUNT 60
 #define PARAMS_NAME_LENGTH MAVLINK_MSG_PARAM_SET_FIELD_PARAM_ID_LEN
 
-/*********************************/
-/*** 0-10 SERIAL CONFIGURATION ***/
-/*********************************/
+typedef enum {
 
-#define PARAM_SYSTEM_ID 0
-#define PARAM_STREAM_HEARTBEAT_RATE 1
-#define PARAM_STREAM_IMU_RATE 2
-#define PARAM_STREAM_MAG_RATE 3
-#define PARAM_STREAM_AIRSPEED_RATE 4
-#define PARAM_STREAM_GPS_RATE 5
-#define PARAM_STREAM_SONAR_RATE 6
-#define PARAM_LOOPTIME 7
+  /*****************************/
+  /*** MAVLINK CONFIGURATION ***/
+  /*****************************/
 
-/***********************/
-/*** 11-30 PID GAINS ***/
-/***********************/
+  PARAM_SYSTEM_ID,
+  PARAM_STREAM_HEARTBEAT_RATE,
+  PARAM_STREAM_IMU_RATE,
+  PARAM_STREAM_MAG_RATE,
+  PARAM_STREAM_AIRSPEED_RATE,
+  PARAM_STREAM_GPS_RATE,
+  PARAM_STREAM_SONAR_RATE,
 
-#define PARAM_PID_ROLL_RATE_P 11
-#define PARAM_PID_ROLL_RATE_I 12
-#define PARAM_PID_ROLL_RATE_D 13
+  /****************************/
+  /*** SYSTEM CONFIGURATION ***/
+  /****************************/
 
-#define PARAM_PID_PITCH_RATE_P 14
-#define PARAM_PID_PITCH_RATE_I 15
-#define PARAM_PID_PITCH_RATE_D 16
+  PARAM_LOOPTIME,
 
-#define PARAM_PID_YAW_RATE_P 17
-#define PARAM_PID_YAW_RATE_I 18
-#define PARAM_PID_YAW_RATE_D 19
+  /*****************/
+  /*** PID GAINS ***/
+  /*****************/
 
-#define PARAM_PID_ROLL_ANGLE_P 20
-#define PARAM_PID_ROLL_ANGLE_I 21
-#define PARAM_PID_ROLL_ANGLE_D 22
+  PARAM_PID_ROLL_RATE_P,
+  PARAM_PID_ROLL_RATE_I,
+  PARAM_PID_ROLL_RATE_D,
 
-#define PARAM_PID_PITCH_ANGLE_P 23
-#define PARAM_PID_PITCH_ANGLE_I 24
-#define PARAM_PID_PITCH_ANGLE_D 25
+  PARAM_PID_PITCH_RATE_P,
+  PARAM_PID_PITCH_RATE_I,
+  PARAM_PID_PITCH_RATE_D,
 
-#define PARAM_PID_ALT_P 26
-#define PARAM_PID_ALT_I 27
-#define PARAM_PID_ALT_D 28
+  PARAM_PID_YAW_RATE_P,
+  PARAM_PID_YAW_RATE_I,
+  PARAM_PID_YAW_RATE_D,
 
-/*******************************/
-/*** 31-40 PWM CONFIGURATION ***/
-/*******************************/
+  PARAM_PID_ROLL_ANGLE_P,
+  PARAM_PID_ROLL_ANGLE_I,
+  PARAM_PID_ROLL_ANGLE_D,
 
-#define PARAM_MOTOR_PWM_SEND_RATE 31
+  PARAM_PID_PITCH_ANGLE_P,
+  PARAM_PID_PITCH_ANGLE_I,
+  PARAM_PID_PITCH_ANGLE_D,
 
-#define PARAM_RC_ROLL_CHANNEL 32
-#define PARAM_RC_PITCH_CHANNEL 33
-#define PARAM_RC_YAW_CHANNEL 34
-#define PARAM_RC_THROTTLE_CHANNEL 35
-#define PARAM_RC_TYPE 36 // 0 is PWM, 1 is PPM
-#define PARAM_IDLE_PWM 37
+  PARAM_PID_ALT_P,
+  PARAM_PID_ALT_I,
+  PARAM_PID_ALT_D,
 
-/*************************************/
-/*** 41-50 ESTIMATOR CONFIGURATION ***/
-/*************************************/
+  /*************************/
+  /*** PWM CONFIGURATION ***/
+  /*************************/
 
-#define PARAM_ESTIMATOR_LPF_ALPHA 41
-#define PARAM_GYRO_LPF_ALPHA 42
-#define PARAM_ACC_LPF_ALPHA 43
+  PARAM_MOTOR_PWM_SEND_RATE,
+
+  PARAM_RC_ROLL_CHANNEL,
+  PARAM_RC_PITCH_CHANNEL,
+  PARAM_RC_YAW_CHANNEL,
+  PARAM_RC_THROTTLE_CHANNEL,
+  PARAM_RC_TYPE, // 0 is PWM, 1 is PPM
+  PARAM_IDLE_PWM,
+
+  /*******************************/
+  /*** ESTIMATOR CONFIGURATION ***/
+  /*******************************/
+
+  PARAM_ESTIMATOR_LPF_ALPHA,
+  PARAM_GYRO_LPF_ALPHA,
+  PARAM_ACC_LPF_ALPHA,
+
+  // keep track of size of params array
+  PARAMS_COUNT = PARAM_ACC_LPF_ALPHA
+} paramId_t;
 
 // type definitions
 typedef struct
@@ -98,14 +108,14 @@ void init_params(void);
  * @brief Callback for executing actions that need to be taken when a parameter value changes
  * @param id The ID of the parameter that was changed
  */
-void param_change_callback(uint8_t id);
+void param_change_callback(paramId_t id);
 
 /**
  * @brief Gets the id of a parameter from its name
  * @param name The name of the parameter
  * @returns The ID of the parameter if the name is valid, PARAMS_COUNT otherwise (invalid ID)
  */
-uint8_t lookup_param_id(const char name[PARAMS_NAME_LENGTH]);
+paramId_t lookup_param_id(const char name[PARAMS_NAME_LENGTH]);
 
 /**
  * @brief Sets the value of a parameter by ID and calls the parameter change callback
@@ -113,7 +123,7 @@ uint8_t lookup_param_id(const char name[PARAMS_NAME_LENGTH]);
  * @param value The new value
  * @returns True if a parameter value was changed, false otherwise
  */
-inline bool set_param_by_id(uint8_t id, int32_t value)
+inline bool set_param_by_id(paramId_t id, int32_t value)
 {
   if (id < PARAMS_COUNT && value != _params.values[id])
   {
--- src/param.c
+++ src/param.c
@@ -12,7 +12,7 @@
 params_t _params;
 
 // local function definitions
-static void init_param(uint8_t id, char name[PARAMS_NAME_LENGTH], int32_t value)
+static void init_param(paramId_t id, char name[PARAMS_NAME_LENGTH], int32_t value)
 {
   _params.values[id] = value;
   strcpy(_params.names[id], name);
@@ -24,18 +24,22 @@ void init_params(void)
   initEEPROM();
   if(!readEEPROM())
   {
-      init_param(PARAM_SYSTEM_ID, "SYS_ID", 1);
-      init_param(PARAM_STREAM_HEARTBEAT_RATE, "STRM_HRTBT", 1);
-      init_param(PARAM_STREAM_IMU_RATE, "STRM_IMU", 100);
+    init_param(PARAM_SYSTEM_ID, "SYS_ID", 1);
+    init_param(PARAM_STREAM_HEARTBEAT_RATE, "STRM_HRTBT", 1);
+    init_param(PARAM_STREAM_IMU_RATE, "STRM_IMU", 100);
 
-      for (uint8_t id = 0; id < PARAMS_COUNT; id++)
-        param_change_callback(id);
+    // temporary: replace with actual initialisation of rest of params
+    for (paramId_t id = 3; id < PARAMS_COUNT; id++)
+      init_param(id, "NULL", -1);
 
-      writeEEPROM(true);
+    for (paramId_t id = 0; id < PARAMS_COUNT; id++)
+      param_change_callback(id);
+
+    writeEEPROM(true);
   }
 }
 
-void param_change_callback(uint8_t id)
+void param_change_callback(paramId_t id)
 {
   switch (id)
   {
@@ -54,9 +58,9 @@ void param_change_callback(uint8_t id)
   }
 }
 
-uint8_t lookup_param_id(const char name[PARAMS_NAME_LENGTH])
+paramId_t lookup_param_id(const char name[PARAMS_NAME_LENGTH])
 {
-  for (uint8_t id = 0; id < PARAMS_COUNT; id++)
+  for (paramId_t id = 0; id < PARAMS_COUNT; id++)
   {
     bool match = true;
     for (uint8_t i = 0; i < PARAMS_NAME_LENGTH; i++)
