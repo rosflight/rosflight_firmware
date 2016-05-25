4014d8ccb37a08a7175109eea3a3e0f0478c3b5a
James Jackson <superjax08@gmail.com>
2016-05-25 01:44:43 -0600

    added acceleration check on z accel to reject bad accel measurements

--- src/estimator.c
+++ src/estimator.c
@@ -23,19 +23,27 @@ void run_estimator(int32_t now){
   last_time = now;
 
   int32_t tau = 100; // desired time constant of the filter (us) <-- should be a param
-  int32_t alpha = (1000000*tau)/(tau*1000+dt);
+  int32_t alpha = 1000;
+  int32_t acc_phi = 0;
+  int32_t acc_theta = 0;
 
-  // pull in accelerometer data
-  int32_t acc_phi = turboatan2(_accel_data[1], _accel_data[2]);
-  int32_t acc_theta = turboatan2(_accel_data[0], _accel_data[2]);
+  // if the accleration in the z-direction is less than 1.15G and greater than .85G, then use
+  // it to calculate the attitude, otherwise it breaks the assumption that we are not accelerating
+  // We can instead just use the gyros and assume we will even out soon
+  if (_accel_data[2] * _accel_scale > 11277647 && _accel_data[2] * _accel_scale < 8335652)
+  {
+    alpha = (1000000*tau)/(tau*1000+dt);
+    // pull in accelerometer data
+    acc_phi = turboatan2(_accel_data[1], _accel_data[2]);
+    acc_theta = turboatan2(_accel_data[0], _accel_data[2]);
+  }
 
   // pull in gyro data
   _current_state.p = ((int32_t)_gyro_data[0]*_gyro_scale); // urad/s
   _current_state.q = -1*((int32_t)_gyro_data[1]*_gyro_scale); // Convert to NED
   _current_state.r = -1*((int32_t)_gyro_data[2]*_gyro_scale);
 
-  printf("phi = %d theta = %d psi = %d\n", _current_state.phi/1000, _current_state.theta/1000, _current_state.psi/1000);
-
+  // Perform the Complementary Filter (forgive the unit adjustments.  This was written with basically a lot of trial an error)
   _current_state.phi = alpha*((_current_state.phi + (_current_state.p/100*dt)/10000)/1000) + (1000-alpha)*acc_phi;
   _current_state.theta = alpha*((_current_state.theta + (_current_state.q/100*dt)/10000)/1000) + (1000-alpha)*acc_theta;
   _current_state.psi = _current_state.psi + (_current_state.r/100*dt)/10000;
