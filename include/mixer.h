2205007d87f187ebd05cfdae45d7e6e97dec828e
James Jackson <superjax08@gmail.com>
2016-05-25 00:17:32 -0600

    add saturation to mixer

--- include/mixer.h
+++ include/mixer.h
@@ -26,18 +26,18 @@ typedef struct {
 
 typedef struct {
   output_type_t output_type[8];
-  int16_t F[8];
-  int16_t x[8];
-  int16_t y[8];
-  int16_t z[8];
+  int32_t F[8];
+  int32_t x[8];
+  int32_t y[8];
+  int32_t z[8];
 } mixer_t;
 
 extern command_t _command;
 
-extern int16_t _GPIO_outputs[8];
+extern int32_t _GPIO_outputs[8];
 extern output_type_t _GPIO_output_type[8];
 
-extern int16_t _outputs[8];
+extern int32_t _outputs[8];
 
 void init_mixing();
 void mix_output();
--- src/mixer.c
+++ src/mixer.c
@@ -4,8 +4,8 @@
 
 #include "mixer.h"
 
-int16_t _GPIO_outputs[8];
-int16_t _outputs[8];
+int32_t _GPIO_outputs[8];
+int32_t _outputs[8];
 command_t _command;
 output_type_t _GPIO_output_type[8];
 
@@ -84,65 +84,89 @@ void init_mixing()
 }
 
 
-void write_motor(uint8_t index, int16_t value){
+void write_motor(uint8_t index, int32_t value){
   /** TODO:
    * reverse the PWM -> mRad conversion in RC
    */
 
-  if (value > 2000)
+  if (value > 1000)
   {
-    value = 2000;
+    value = 1000;
   }
   else if (value < 0)
   {
     value = 0;
   }
-  pwmWriteMotor(index, value);
+  pwmWriteMotor(index, value+2000);
 }
 
-void write_servo(uint8_t index, int16_t value){
+void write_servo(uint8_t index, int32_t value){
   /** TODO:
    * reverse the PWM conversion in RC (if any)
    */
 
-  if (value > 2000)
+  if (value > 500)
   {
-    value = 2000;
+    value = 500;
   }
-  else if (value < 0)
+  else if (value < -500)
   {
-    value = 0;
+    value = -500;
   }
-  pwmWriteMotor(index, value);
+  pwmWriteMotor(index, 1500+value);
 }
 
 void mix_output()
 {
   printf("COMMANDS: F = %d, x = %d, y = %d, z = %d\n", _command.F, _command.x, _command.y, _command.z);
   // Mix Output
+  int32_t max_output = 0;
   for (int8_t i=0; i<8; i++)
   {
-    output_type_t output_type = mixer_to_use.output_type[i];
-    if(output_type != NONE)
+    if (mixer_to_use.output_type[i] != NONE)
     {
       // Matrix multiply (in so many words) -- done in integer, hence the /1000 at the end
       _outputs[i] = (_command.F*mixer_to_use.F[i] + _command.x*mixer_to_use.x[i] +
                     _command.y*mixer_to_use.y[i] + _command.z*mixer_to_use.z[i])/1000;
+      if (_outputs[i] > 1000 && _outputs[i] > max_output)
+      {
+        max_output = _outputs[i];
+      }
       printf("mixer col %d: %d %d %d %d\t", i, mixer_to_use.F[i], mixer_to_use.x[i], mixer_to_use.y[i], mixer_to_use.z[i]);
     }
-    else
+  }
+
+  // saturate outputs to maintain controllability even at high levels of throttle
+  if (max_output > 1000)
+  {
+    int32_t scale_factor = (max_output*1000)/1000;
+    for (int8_t i=0; i<8; i++)
+    {
+      if ( mixer_to_use.output_type[i] == M)
+      {
+        _outputs[i] = (_outputs[i]*1000)/scale_factor; // divide by scale factor
+      }
+    }
+  }
+
+  // Add in GPIO inptus from Onboard Computer
+  for ( int8_t i=0; i<8; i++)
+  {
+    output_type_t output_type = mixer_to_use.output_type[i];
+    if ( output_type == NONE)
     {
       // Incorporate GPIO on not already reserved outputs
       _outputs[i] = _GPIO_outputs[i];
       output_type = _GPIO_output_type[i];
     }
 
+    // Write output to motors
     if (output_type == S)
     {
       write_servo(i, _outputs[i]);
       printf("writing SERVO %d, %d\n", i+1, _outputs[i]);
     }
-    else
+    else if (output_type == M)
     {
       write_motor(i, _outputs[i]);
       printf("writing MOTOR %d: %d\n", i+1, _outputs[i]);
