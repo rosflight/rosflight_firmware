4a11277864b25a7a96bf29f00856f3bd546ee576
Daniel Koch <daniel.p.koch@gmail.com>
2016-09-17 12:01:38 -0600

    Removed unused LOOPTIME parameter

--- include/param.h
+++ include/param.h
@@ -39,7 +39,6 @@ typedef enum
   /****************************/
   /*** SYSTEM CONFIGURATION ***/
   /****************************/
-  PARAM_LOOPTIME,
   PARAM_DIFF_PRESS_UPDATE,
   PARAM_BARO_UPDATE,
   PARAM_SONAR_UPDATE,
