21ffe3301c6b058f58c224e07bb3a6d6e4a807af
James Jackson <superjax08@gmail.com>
2017-03-10 00:18:46 -0700

    mixer class building - All classes are building

--- include/mixer.h
+++ include/mixer.h
@@ -1,94 +1,165 @@
-/* 
- * Copyright (c) 2017, James Jackson and Daniel Koch, BYU MAGICC Lab
- * 
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- * * Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- * 
- * * Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- * 
- * * Neither the name of the copyright holder nor the names of its
- *   contributors may be used to endorse or promote products derived from
- *   this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #pragma once
 
 #include <stdint.h>
 #include <stdbool.h>
 
+#include "board.h"
+#include "mux.h"
+#include "param.h"
+#include "arming_fsm.h"
 
-// This enum needs to match the "array_of_mixers" variable in mixer.c
-typedef enum
+namespace rosflight {
+
+class Mixer
 {
-  QUADCOPTER_PLUS,
-  QUADCOPTER_X,
-  QUADCOPTER_H,
-  TRICOPTER,
-  Y6,
-  X8,
-  FIXEDWING,
-  MYTWINDREAM_MIXER,
-  NUM_MIXERS
 
-} mixer_type_t;
+public:
+  typedef enum
+  {
+    QUADCOPTER_PLUS,
+    QUADCOPTER_X,
+    QUADCOPTER_H,
+    TRICOPTER,
+    Y6,
+    FIXEDWING,
+    MYTWINDREAM_MIXER,
+    NUM_MIXERS
 
-typedef enum
-{
-  NONE, // None
-  S, // Servo
-  M, // Motor
-  G // GPIO
-} output_type_t;
+  } mixer_type_t;
 
-typedef struct
-{
-  float F;
-  float x;
-  float y;
-  float z;
-} command_t;
+  typedef enum
+  {
+    NONE, // None
+    S, // Servo
+    M, // Motor
+    G // GPIO
+  } output_type_t;
 
-typedef struct
-{
-  output_type_t output_type[8];
-  float F[8];
-  float x[8];
-  float y[8];
-  float z[8];
-} mixer_t;
+  typedef struct
+  {
+    float F;
+    float x;
+    float y;
+    float z;
+  } command_t;
 
-extern command_t _command;
+  typedef struct
+  {
+    output_type_t output_type[8];
+    float F[8];
+    float x[8];
+    float y[8];
+    float z[8];
+  } mixer_t;
 
-extern float _GPIO_outputs[8];
-extern output_type_t _GPIO_output_type[8];
+  command_t _command;
 
-extern float _outputs[8];
+  float _GPIO_outputs[8];
+  output_type_t _GPIO_output_type[8];
 
-void init_PWM();
-void init_mixing();
-void mix_output();
-#ifdef __cplusplus
+  float _outputs[8];
+
+  void init(Board* _board, Mux* _mux, Params* _params, Arming_FSM* _fsm);
+  void init_PWM();
+  void init_mixing();
+  void mix_output();
+
+private:
+
+  Board* board;
+  Mux* mux;
+  Params* params;
+  Arming_FSM* fsm;
+
+  float prescaled_outputs[8];
+
+  void write_motor(uint8_t index, float value);
+  void write_servo(uint8_t index, float value);
+
+  const mixer_t quadcopter_plus_mixing =
+  {
+    {M, M, M, M, NONE, NONE, NONE, NONE}, // output_type
+
+    { 1.0f,  1.0f,  1.0f,  1.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // F Mix
+    { 0.0f, -1.0f,  1.0f,  0.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // X Mix
+    {-1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // Y Mix
+    {-1.0f,  1.0f,  1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f}  // Z Mix
+  };
+
+
+  const mixer_t quadcopter_x_mixing =
+  {
+    {M, M, M, M, NONE, NONE, NONE, NONE}, // output_type
+
+    { 1.0f, 1.0f, 1.0f, 1.0f,  0.0f, 0.0f, 0.0f, 0.0f}, // F Mix
+    {-1.0f,-1.0f, 1.0f, 1.0f,  0.0f, 0.0f, 0.0f, 0.0f}, // X Mix
+    {-1.0f, 1.0f,-1.0f, 1.0f,  0.0f, 0.0f, 0.0f, 0.0f}, // Y Mix
+    {-1.0f, 1.0f, 1.0f,-1.0f,  0.0f, 0.0f, 0.0f, 0.0f}  // Z Mix
+  };
+
+  const mixer_t quadcopter_h_mixing =
+  {
+    {M, M, M, M, NONE, NONE, NONE, NONE}, // output_type
+
+    { 1.0f, 1.0f, 1.0f, 1.0f,  0.0f, 0.0f, 0.0f, 0.0f}, // F Mix
+    {-1057, -943, 1057,  943,  0.0f, 0.0f, 0.0f, 0.0f}, // X Mix
+    {-1005,  995,-1005,  995,  0.0f, 0.0f, 0.0f, 0.0f}, // Y Mix
+    {-1.0f, 1.0f, 1.0f,-1.0f,  0.0f, 0.0f, 0.0f, 0.0f}  // Z Mix
+  };
+
+  const mixer_t fixedwing_mixing =
+  {
+    {S, S, M, S, S, M, NONE, NONE},
+
+    { 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // F Mix
+    { 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // X Mix
+    { 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // Y Mix
+    { 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f}  // Z Mix
+  };
+
+  const mixer_t mytwindream_mixing =
+  {
+    {S, S, M, S, S, M, NONE, NONE},
+
+    { 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // F Mix
+    { 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // X Mix
+    { 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // Y Mix
+    { 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f}  // Z Mix
+  };
+
+  const mixer_t tricopter_mixing =
+  {
+    {M, M, M, S, NONE, NONE, NONE, NONE},
+
+    { 1.0f,     1.0f,   1.0f,   0.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // F Mix
+    {-1.0f,     1.0f,   0.0f,   0.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // X Mix
+    {-0.667f,  -0.667f, 1.333f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // Y Mix
+    { 0.0f,     0.0f,   0.0f,   1.0f, 0.0f, 0.0f, 0.0f, 0.0f}  // Z Mix
+  };
+
+  const mixer_t Y6_mixing =
+  {
+    {M, M, M, M, M, M, NONE, NONE},
+    { 1.0f,   1.0f,    1.0f,    1.0f,    1.0f,    1.0f,   0.0f, 0.0f}, // F Mix
+    { 0.0f,  -1.0f,    1.0f,    0.0f,   -1.0f,    1.0f,   0.0f, 0.0f}, // X Mix
+    {-1.333f, 0.667f,  0.667f, -1.333f,  0.667f,  0.667f, 0.0f, 0.0f}, // Y Mix
+    {-1.0f,   1.0f,    1.0f,    1.0f,   -1.0f,   -1.0f,   0.0f, 0.0f}  // Z Mix
+  };
+
+  const mixer_t* mixer_to_use;
+
+  const mixer_t *array_of_mixers[NUM_MIXERS] =
+  {
+    &quadcopter_plus_mixing,
+    &quadcopter_x_mixing,
+    &quadcopter_h_mixing,
+    &tricopter_mixing,
+    &Y6_mixing,
+    &fixedwing_mixing,
+    &mytwindream_mixing
+  };
+
+
+};
+
 }
-#endif
--- src/mixer.cpp
+++ src/mixer.cpp
@@ -1,128 +1,22 @@
 #include <stdint.h>
 
-#include "board.h"
 #include "mixer.h"
-#include "param.h"
-#include "mode.h"
-#include "rc.h"
-#include "estimator.h"
 
-#include "mavlink_log.h"
+namespace rosflight {
 
-static float prescaled_outputs[8];
-float _outputs[8];
-command_t _command;
-
-static mixer_t quadcopter_plus_mixing =
+void Mixer::init(Board *_board, Mux *_mux, Params *_params, Arming_FSM *_fsm)
 {
-  {M, M, M, M, NONE, NONE, NONE, NONE}, // output_type
-
-  { 1.0f,  1.0f,  1.0f,  1.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // F Mix
-  { 0.0f, -1.0f,  1.0f,  0.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // X Mix
-  {-1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // Y Mix
-  {-1.0f,  1.0f,  1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f}  // Z Mix
-};
+  board = _board;
+  mux = _mux;
+  params = _params;
+  fsm = _fsm;
+}
 
 
-static mixer_t quadcopter_x_mixing =
+void Mixer::init_mixing()
 {
-  {M, M, M, M, NONE, NONE, NONE, NONE}, // output_type
-
-  { 1.0f, 1.0f, 1.0f, 1.0f,  0.0f, 0.0f, 0.0f, 0.0f}, // F Mix
-  {-1.0f,-1.0f, 1.0f, 1.0f,  0.0f, 0.0f, 0.0f, 0.0f}, // X Mix
-  {-1.0f, 1.0f,-1.0f, 1.0f,  0.0f, 0.0f, 0.0f, 0.0f}, // Y Mix
-  {-1.0f, 1.0f, 1.0f,-1.0f,  0.0f, 0.0f, 0.0f, 0.0f}  // Z Mix
-};
-
-static mixer_t quadcopter_h_mixing =
-{
-  {M, M, M, M, NONE, NONE, NONE, NONE}, // output_type
-
-  { 1.0f, 1.0f, 1.0f, 1.0f,  0.0f, 0.0f, 0.0f, 0.0f}, // F Mix
-  {-1057, -943, 1057,  943,  0.0f, 0.0f, 0.0f, 0.0f}, // X Mix
-  {-1005,  995,-1005,  995,  0.0f, 0.0f, 0.0f, 0.0f}, // Y Mix
-  {-1.0f, 1.0f, 1.0f,-1.0f,  0.0f, 0.0f, 0.0f, 0.0f}  // Z Mix
-};
-
-static mixer_t fixedwing_mixing =
-{
-  {S, S, M, S, S, M, NONE, NONE},
-
-  { 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // F Mix
-  { 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // X Mix
-  { 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // Y Mix
-  { 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f}  // Z Mix
-};
-
-static mixer_t mytwindream_mixing =
-{
-  {S, S, M, S, S, M, NONE, NONE},
-
-  { 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // F Mix
-  { 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // X Mix
-  { 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // Y Mix
-  { 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f}  // Z Mix
-};
-
-static mixer_t tricopter_mixing =
-{
-  {M, M, M, S, NONE, NONE, NONE, NONE},
-
-  { 1.0f,     1.0f,   1.0f,   0.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // F Mix
-  {-1.0f,     1.0f,   0.0f,   0.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // X Mix
-  {-0.667f,  -0.667f, 1.333f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f}, // Y Mix
-  { 0.0f,     0.0f,   0.0f,   1.0f, 0.0f, 0.0f, 0.0f, 0.0f}  // Z Mix
-};
-
-static mixer_t Y6_mixing =
-{
-  {M, M, M, M, M, M, NONE, NONE},
-  { 1.0f,   1.0f,    1.0f,    1.0f,    1.0f,    1.0f,   0.0f, 0.0f}, // F Mix
-  {-1.0f,  -1.0f,    0.0f,    0.0f,    1.0f,    1.0f,   0.0f, 0.0f}, // X Mix
-  { 0.667f, 0.667f, -1.333f, -1.333f,  0.667f,  0.667f, 0.0f, 0.0f}, // Y Mix
-  {-1.0f,   1.0f,   -1.0f,    1.0f,   -1.0f,    1.0f,   0.0f, 0.0f}  // Z Mix
-};
-
-static mixer_t X8_mixing =
-{
-  {M, M, M, M, M, M, M, M},
-  { 1.0f,   1.0f,    1.0f,    1.0f,    1.0f,    1.0f,   1.0f,  1.0f}, // F Mix
-  {-1.0f,   1.0f,    1.0f,   -1.0f,    1.0f,   -1.0f,  -1.0f,  1.0f}, // X Mix
-  { 1.0f,   1.0f,   -1.0f,   -1.0f,    1.0f,    1.0f,  -1.0f, -1.0f}, // Y Mix
-  { 1.0f,  -1.0f,    1.0f,   -1.0f,    1.0f,   -1.0f,   1.0f, -1.0f}  // Z Mix
-};
-
-static mixer_t* mixer_to_use;
-
-static mixer_t *array_of_mixers[NUM_MIXERS] =
-{
-  &quadcopter_plus_mixing,
-  &quadcopter_x_mixing,
-  &quadcopter_h_mixing,
-  &tricopter_mixing,
-  &Y6_mixing,
-  &fixedwing_mixing,
-  &mytwindream_mixing
-};
-
-
-
-void init_mixing()
-{
-  // clear the invalid mixer flag
-  _error_state &= ~(ERROR_INVALID_MIXER);
-
-  uint8_t mixer_choice = get_param_int(PARAM_MIXER);
-
-  if (mixer_choice >= NUM_MIXERS)
-  {
-    mavlink_log_error("Invalid Mixer Choice", NULL);
-    mixer_choice = 0;
-    // set the invalid mixer flag
-    _error_state |= ERROR_INVALID_MIXER;
-  }
-
-  mixer_to_use = array_of_mixers[get_param_int(PARAM_MIXER)];
+  // We need a better way to choosing the mixer
+  mixer_to_use = array_of_mixers[params->get_param_int(PARAM_MIXER)];
 
   for (int8_t i=0; i<8; i++)
   {
@@ -135,30 +29,30 @@ void init_mixing()
   _command.z = 0;
 }
 
-void init_PWM()
+void Mixer::init_PWM()
 {
   bool useCPPM = false;
-  if (get_param_int(PARAM_RC_TYPE) == 1)
+  if (params->get_param_int(PARAM_RC_TYPE) == 1)
   {
     useCPPM = true;
   }
-  int16_t motor_refresh_rate = get_param_int(PARAM_MOTOR_PWM_SEND_RATE);
-  int16_t off_pwm = get_param_int(PARAM_MOTOR_MIN_PWM);
-  pwm_init(useCPPM, motor_refresh_rate, off_pwm);
+  int16_t motor_refresh_rate = params->get_param_int(PARAM_MOTOR_PWM_SEND_RATE);
+  int16_t off_pwm = params->get_param_int(PARAM_MOTOR_MIN_PWM);
+  board->pwm_init(useCPPM, motor_refresh_rate, off_pwm);
 }
 
 
-void write_motor(uint8_t index, float value)
+void Mixer::write_motor(uint8_t index, float value)
 {
-  if (_armed_state & ARMED)
+  if (fsm->_armed_state == ARMED)
   {
     if (value > 1.0)
     {
       value = 1.0;
     }
-    else if (value < get_param_float(PARAM_MOTOR_IDLE_THROTTLE) && get_param_int(PARAM_SPIN_MOTORS_WHEN_ARMED))
+    else if (value < params->get_param_float(PARAM_MOTOR_IDLE_THROTTLE) && params->get_param_int(PARAM_SPIN_MOTORS_WHEN_ARMED))
     {
-      value = get_param_float(PARAM_MOTOR_IDLE_THROTTLE);
+      value = params->get_param_int(PARAM_MOTOR_IDLE_THROTTLE);
     }
     else if (value < 0.0)
     {
@@ -170,12 +64,12 @@ void write_motor(uint8_t index, float value)
     value = 0.0;
   }
   _outputs[index] = value;
-  int32_t pwm_us = value * (get_param_int(PARAM_MOTOR_MAX_PWM) - get_param_int(PARAM_MOTOR_MIN_PWM)) + get_param_int(PARAM_MOTOR_MIN_PWM);
-  pwm_write(index, pwm_us);
+  int32_t pwm_us = value * (params->get_param_int(PARAM_MOTOR_MAX_PWM) - params->get_param_int(PARAM_MOTOR_MIN_PWM)) + params->get_param_int(PARAM_MOTOR_MIN_PWM);
+  board->pwm_write(index, pwm_us);
 }
 
 
-void write_servo(uint8_t index, float value)
+void Mixer::write_servo(uint8_t index, float value)
 {
   if (value > 1.0)
   {
@@ -186,11 +80,11 @@ void write_servo(uint8_t index, float value)
     value = -1.0;
   }
   _outputs[index] = value;
-  pwm_write(index, _outputs[index] * 500 + 1500);
+  board->pwm_write(index, _outputs[index] * 1000 + 1500);
 }
 
 
-void mix_output()
+void Mixer::mix_output()
 {
   float max_output = 1.0f;
 
@@ -225,9 +119,30 @@ void mix_output()
     scale_factor = 1.0/max_output;
   }
 
+  // Reverse Fixedwing channels
+  if (params->get_param_int(PARAM_FIXED_WING))
+  {
+    prescaled_outputs[0] *= params->get_param_int(PARAM_AILERON_REVERSE) ? -1 : 1;
+    prescaled_outputs[1] *= params->get_param_int(PARAM_ELEVATOR_REVERSE) ? -1 : 1;
+    prescaled_outputs[3] *= params->get_param_int(PARAM_RUDDER_REVERSE) ? -1 : 1;
+  }
+
   // Add in GPIO inputs from Onboard Computer
   for (int8_t i=0; i<8; i++)
   {
+    output_type_t output_type = mixer_to_use->output_type[i];
+    if (mixer_to_use->output_type[i] == M)
+    {
+      prescaled_outputs[i] = (prescaled_outputs[i])*scale_factor; // scale all outputs by scale factor
+    }
+
+    if (output_type == NONE)
+    {
+      // Incorporate GPIO on not already reserved outputs
+      prescaled_outputs[i] = _GPIO_outputs[i];
+      output_type = _GPIO_output_type[i];
+    }
+
     // Write output to motors
     if (mixer_to_use->output_type[i] == S)
     {
@@ -240,4 +155,6 @@ void mix_output()
       write_motor(i, prescaled_outputs[i]);
     }
   }
+}
+
 }
