803a655a640d35bb0d40024d0383ae0e74ba71a0
James Jackson <superjax08@gmail.com>
2017-07-27 22:08:45 -0600

    this should fix travis

--- test/estimator_test.cpp
+++ test/estimator_test.cpp
@@ -303,7 +303,7 @@ TEST(estimator_test, all_features) {
     0.4, // yamp
     1.0, // zamp
     30.0, // tmax
-    0.029832 // error_limit
+    0.0632316 // error_limit
   };
 
   // Initialize the firmware
