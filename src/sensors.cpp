b72426541d697890722647b1523293bcff8c9846
James Jackson <superjax08@gmail.com>
2017-08-30 10:34:16 -0600

    added fc_orientation parameters and fix IMU not responding error always being printed when disarmed

--- src/sensors.cpp
+++ src/sensors.cpp
@@ -283,7 +283,8 @@ bool Sensors::update_imu(void)
   else
   {
     // if we have lost 10 IMU messages then something is wrong
-    if (rf_.board_.clock_millis() > last_imu_update_ms_ + 10)
+    int imu_timeout = rf_.state_manager_.state().armed ? 10 : 1000;
+    if (rf_.board_.clock_millis() > last_imu_update_ms_ + imu_timeout)
     {
       // Tell the board to fix it
       last_imu_update_ms_ = rf_.board_.clock_millis();
