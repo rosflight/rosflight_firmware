f401a260f5e7f9b562f1e0bf1dd047150447be4d
Parker Lusk <parkerclusk@gmail.com>
2019-07-25 16:30:25 -0400

    estimator cleanup

--- src/estimator.cpp
+++ src/estimator.cpp
@@ -126,8 +126,12 @@ void Estimator::set_external_attitude_update(const turbomath::Quaternion &q)
 
 void Estimator::run()
 {
-  float kp, ki;
-  uint64_t now_us = RF_.sensors_.data().imu_time;
+
+  //
+  // Timing Setup
+  //
+
+  const uint64_t now_us = RF_.sensors_.data().imu_time;
   if (last_time_ == 0)
   {
     last_time_ = now_us;
@@ -143,76 +147,39 @@ void Estimator::run()
     return;
   }
 
-
   RF_.state_manager_.clear_error(StateManager::ERROR_TIME_GOING_BACKWARDS);
 
   float dt = (now_us - last_time_) * 1e-6f;
   last_time_ = now_us;
   state_.timestamp_us = now_us;
 
-  // Crank up the gains for the first few seconds for quick convergence
-  if (now_us < static_cast<uint64_t>(RF_.params_.get_param_int(PARAM_INIT_TIME))*1000)
-  {
-    kp = RF_.params_.get_param_float(PARAM_FILTER_KP_ACC)*10.0f;
-    ki = RF_.params_.get_param_float(PARAM_FILTER_KI)*10.0f;
-  }
-  else
-  {
-    kp = RF_.params_.get_param_float(PARAM_FILTER_KP_ACC);
-    ki = RF_.params_.get_param_float(PARAM_FILTER_KI);
-  }
-
-  // Run LPF to reject a lot of noise
+  // Low-pass filter accel and gyro measurements
   run_LPF();
 
-  // add in accelerometer
-  float a_sqrd_norm = accel_LPF_.sqrd_norm();
+  //
+  // Gyro Correction Term (werr)
+  //
 
-  // establish allowed acceleration deviation from 1g (i.e., non-accelerated flight)
-  const float margin = RF_.params_.get_param_float(PARAM_FILTER_ACCEL_MARGIN);
-  const float lowerbound = (1.0f - margin)*(1.0f - margin)*9.80665f*9.80665f;
-  const float upperbound = (1.0f + margin)*(1.0f + margin)*9.80665f*9.80665f;
+  float kp = 0.0f;
+  float ki = RF_.params_.get_param_float(PARAM_FILTER_KI);
 
   turbomath::Vector w_err;
-  if (RF_.params_.get_param_int(PARAM_FILTER_USE_ACC)
-      && lowerbound < a_sqrd_norm && a_sqrd_norm < upperbound)
+
+  if (can_use_accel())
   {
     // Get error estimated by accelerometer measurement
+    w_err = accel_correction();
+    kp = RF_.params_.get_param_float(PARAM_FILTER_KP_ACC);
+
     last_acc_update_us_ = now_us;
-    // turn measurement into a unit vector
-    turbomath::Vector a = accel_LPF_.normalized();
-    // Get the quaternion from accelerometer (low-frequency measure q)
-    // (Not in either paper)
-    turbomath::Quaternion q_acc_inv(g_, a);
-    // Get the error quaternion between observer and low-freq q
-    // Below Eq. 45 Mahony Paper
-    turbomath::Quaternion q_tilde = q_acc_inv * state_.attitude;
-    // Correction Term of Eq. 47a and 47b Mahony Paper
-    // w_err = 2*s_tilde*v_tilde
-    w_err.x = -2.0f*q_tilde.w*q_tilde.x;
-    w_err.y = -2.0f*q_tilde.w*q_tilde.y;
-    w_err.z = 0.0f; // Don't correct z, because it's unobservable from the accelerometer
   }
 
-  if (extatt_update_next_run_)
+  if (can_use_extatt())
   {
-    turbomath::Vector xhat_BW, yhat_BW, zhat_BW;
-    turbomath::Vector xext_BW, yext_BW, zext_BW;
-    {
-      // create DCM from quaternion attitude estimate
-      float &w = state_.attitude.w, &x = state_.attitude.x, &y = state_.attitude.y, &z = state_.attitude.z;
-      xhat_BW.x = 1.0f - 2.0f*(y*y + z*z); xhat_BW.y = 2.0f*(x*y - z*w); xhat_BW.z = 2.0f*(x*z + y*w);
-      yhat_BW.x = 2.0f*(x*y + z*w); yhat_BW.y = 1.0f - 2.0f*(x*x + z*z); yhat_BW.z = 2.0f*(y*z - x*w);
-      zhat_BW.x = 2.0f*(x*z - y*w); zhat_BW.y = 2.0f*(y*z + x*w); zhat_BW.z = 1.0f - 2.0f*(x*x + y*y);
-    }
-    {
-      // create DCM from quaternion external attitude
-      float &w = q_extatt_.w, &x = q_extatt_.x, &y = q_extatt_.y, &z = q_extatt_.z;
-      xext_BW.x = 1.0f - 2.0f*(y*y + z*z); xext_BW.y = 2.0f*(x*y - z*w); xext_BW.z = 2.0f*(x*z + y*w);
-      yext_BW.x = 2.0f*(x*y + z*w); yext_BW.y = 1.0f - 2.0f*(x*x + z*z); yext_BW.z = 2.0f*(y*z - x*w);
-      zext_BW.x = 2.0f*(x*z - y*w); zext_BW.y = 2.0f*(y*z + x*w); zext_BW.z = 1.0f - 2.0f*(x*x + y*y);
-    }
-    turbomath::Vector w_ext = xext_BW.cross(xhat_BW) + yext_BW.cross(yhat_BW) + zext_BW.cross(zhat_BW);
+    // Get error estimated by external attitude measurement. Overwrite any
+    // correction based on the accelerometer (assumption: extatt is better).
+    w_err = extatt_correction();
+    kp = RF_.params_.get_param_float(PARAM_FILTER_KP_EXT);
 
     // the angular rate correction from external attitude updates occur at a
     // different rate than IMU updates, so it needs to be integrated with a
@@ -220,21 +187,145 @@ void Estimator::run()
     // ratio so that it is integrated correctly.
     const float extAttDt = (now_us - last_extatt_update_us_) * 1e-6f;
     const float scaleDt = (dt > 0) ? (extAttDt / dt) : 0.0f;
-    w_ext *= scaleDt;
-
-    // use error calculated from external attitude update only
-    w_err = w_ext;
-    kp = RF_.params_.get_param_float(PARAM_FILTER_KP_EXT);
+    w_err *= scaleDt;
 
     last_extatt_update_us_ = now_us;
     extatt_update_next_run_ = false;
   }
 
-  // integrate biases driven by measured angular error
+  // Crank up the gains for the first few seconds for quick convergence
+  if (now_us < static_cast<uint64_t>(RF_.params_.get_param_int(PARAM_INIT_TIME))*1000)
+  {
+    kp = RF_.params_.get_param_float(PARAM_FILTER_KP_ACC)*10.0f;
+    ki = RF_.params_.get_param_float(PARAM_FILTER_KI)*10.0f;
+  }
+
+  //
+  // Composite Bias-Free Angular Rate (wfinal)
+  //
+
+  // Integrate biases driven by measured angular error
   // eq 47b Mahony Paper, using correction term w_err found above
   bias_ -= ki*w_err*dt;
 
-  // Handle Gyro Measurements
+  // Build the composite omega vector for kinematic propagation
+  // This the stuff inside the p function in eq. 47a - Mahony Paper
+  turbomath::Vector wbar = smoothed_gyro_measurement();
+  turbomath::Vector wfinal = wbar - bias_ + kp * w_err;
+
+  //
+  // Propagate Dynamics
+  //
+
+  integrate_angular_rate(state_.attitude, wfinal, dt);
+
+  //
+  // Post-Processing
+  //
+
+  // Extract Euler Angles for controller
+  state_.attitude.get_RPY(&state_.roll, &state_.pitch, &state_.yaw);
+
+  // Save off adjust gyro measurements with estimated biases for control
+  state_.angular_velocity = gyro_LPF_ - bias_;
+
+  // If it has been more than 0.5 seconds since the accel update ran and we
+  // are supposed to be getting them then trigger an unhealthy estimator error.
+  if (RF_.params_.get_param_int(PARAM_FILTER_USE_ACC) && now_us > 500000 + last_acc_update_us_
+      && !RF_.params_.get_param_int(PARAM_FIXED_WING))
+  {
+    RF_.state_manager_.set_error(StateManager::ERROR_UNHEALTHY_ESTIMATOR);
+  }
+  else
+  {
+    RF_.state_manager_.clear_error(StateManager::ERROR_UNHEALTHY_ESTIMATOR);
+  }
+}
+
+bool Estimator::can_use_accel() const
+{
+  // if we are not using accel, just bail
+  if (!RF_.params_.get_param_int(PARAM_FILTER_USE_ACC)) return false;
+
+  // current magnitude of LPF'd accelerometer
+  const float a_sqrd_norm = accel_LPF_.sqrd_norm();
+
+  // Ideally, gyros would never drift and we would never have to use the accelerometer.
+  // Since gyros do drift, we can use the accelerometer (in a non-accelerated state) as
+  // another estimate of roll/pitch angles and to make gyro biases observable (except r).
+  // Since there is noise, we give some margin to what a "non-accelerated state" means.
+  // Establish allowed acceleration deviation from 1g (i.e., non-accelerated flight).
+  const float margin = RF_.params_.get_param_float(PARAM_FILTER_ACCEL_MARGIN);
+  const float lowerbound = (1.0f - margin)*(1.0f - margin)*9.80665f*9.80665f;
+  const float upperbound = (1.0f + margin)*(1.0f + margin)*9.80665f*9.80665f;
+
+  // if the magnitude of the accel measurement is close to 1g, we can use the
+  // accelerometer to correct roll and pitch and estimate gyro biases.
+  return (lowerbound < a_sqrd_norm && a_sqrd_norm < upperbound);
+}
+
+bool Estimator::can_use_extatt() const
+{
+  return extatt_update_next_run_;
+}
+
+turbomath::Vector Estimator::accel_correction() const
+{
+  // turn measurement into a unit vector
+  turbomath::Vector a = accel_LPF_.normalized();
+
+  // Get the quaternion from accelerometer (low-frequency measure q)
+  // (Not in either paper)
+  turbomath::Quaternion q_acc_inv(g_, a);
+
+  // Get the error quaternion between observer and low-freq q
+  // Below Eq. 45 Mahony Paper
+  turbomath::Quaternion q_tilde = q_acc_inv * state_.attitude;
+
+  // Correction Term of Eq. 47a and 47b Mahony Paper
+  // w_acc = 2*s_tilde*v_tilde
+  turbomath::Vector w_acc;
+  w_acc.x = -2.0f*q_tilde.w*q_tilde.x;
+  w_acc.y = -2.0f*q_tilde.w*q_tilde.y;
+  w_acc.z = 0.0f; // Don't correct z, because it's unobservable from the accelerometer
+
+  return w_acc;
+}
+
+turbomath::Vector Estimator::extatt_correction() const
+{
+  // DCM columns of attitude estimate and external measurement (world w.r.t body).
+  // These are the world axes from the perspective of the body frame.
+  turbomath::Vector xhat_BW, yhat_BW, zhat_BW;
+  turbomath::Vector xext_BW, yext_BW, zext_BW;
+
+  // create DCM from quaternion attitude estimate
+  {
+    const float &w = state_.attitude.w, &x = state_.attitude.x, &y = state_.attitude.y, &z = state_.attitude.z;
+    xhat_BW.x = 1.0f - 2.0f*(y*y + z*z); xhat_BW.y = 2.0f*(x*y - z*w); xhat_BW.z = 2.0f*(x*z + y*w);
+    yhat_BW.x = 2.0f*(x*y + z*w); yhat_BW.y = 1.0f - 2.0f*(x*x + z*z); yhat_BW.z = 2.0f*(y*z - x*w);
+    zhat_BW.x = 2.0f*(x*z - y*w); zhat_BW.y = 2.0f*(y*z + x*w); zhat_BW.z = 1.0f - 2.0f*(x*x + y*y);
+  }
+
+  // create DCM from quaternion external attitude
+  {
+    const float &w = q_extatt_.w, &x = q_extatt_.x, &y = q_extatt_.y, &z = q_extatt_.z;
+    xext_BW.x = 1.0f - 2.0f*(y*y + z*z); xext_BW.y = 2.0f*(x*y - z*w); xext_BW.z = 2.0f*(x*z + y*w);
+    yext_BW.x = 2.0f*(x*y + z*w); yext_BW.y = 1.0f - 2.0f*(x*x + z*z); yext_BW.z = 2.0f*(y*z - x*w);
+    zext_BW.x = 2.0f*(x*z - y*w); zext_BW.y = 2.0f*(y*z + x*w); zext_BW.z = 1.0f - 2.0f*(x*x + y*y);
+  }
+
+  // calculate cross products of corresponding axes as an error metric. For example, if vehicle were
+  // level but extatt had a different yaw angle than the internal estimate, xext_BW.cross(xhat_BW)
+  // would be a measure of how the filter needs to update in order to the internal estimate's yaw
+  // to closer to the extatt measurement. This is done for each axis.
+  turbomath::Vector w_ext = xext_BW.cross(xhat_BW) + yext_BW.cross(yhat_BW) + zext_BW.cross(zhat_BW);
+
+  return w_ext;
+}
+
+turbomath::Vector Estimator::smoothed_gyro_measurement()
+{
   turbomath::Vector wbar;
   if (RF_.params_.get_param_int(PARAM_FILTER_USE_QUAD_INT))
   {
@@ -249,66 +340,48 @@ void Estimator::run()
     wbar = gyro_LPF_;
   }
 
-  // Build the composite omega vector for kinematic propagation
-  // This the stuff inside the p function in eq. 47a - Mahony Paper
-  turbomath::Vector wfinal = wbar - bias_ + kp * w_err;
+  return wbar;
+}
 
-  // Propagate Dynamics (only if we've moved)
-  float sqrd_norm_w = wfinal.sqrd_norm();
-  if (sqrd_norm_w > 0.0f)
+void Estimator::integrate_angular_rate(turbomath::Quaternion& quat,
+                        const turbomath::Vector& omega, const float dt) const
+{
+  // only propagate if we've moved
+  // TODO[PCL]: Will this ever be true? We should add a margin to this
+  const float sqrd_norm_w = omega.sqrd_norm();
+  if (sqrd_norm_w == 0.0f) return;
+
+  // for convenience
+  const float &p = omega.x, &q = omega.y, &r = omega.z;
+
+  if (RF_.params_.get_param_int(PARAM_FILTER_USE_MAT_EXP))
   {
-    float p = wfinal.x;
-    float q = wfinal.y;
-    float r = wfinal.z;
-
-    if (RF_.params_.get_param_int(PARAM_FILTER_USE_MAT_EXP))
-    {
-      // Matrix Exponential Approximation (From Attitude Representation and Kinematic
-      // Propagation for Low-Cost UAVs by Robert T. Casey)
-      // (Eq. 12 Casey Paper)
-      // This adds 90 us on STM32F10x chips
-      float norm_w = sqrtf(sqrd_norm_w);
-      turbomath::Quaternion qhat_np1;
-      float t1 = cosf((norm_w*dt)/2.0f);
-      float t2 = 1.0f/norm_w * sinf((norm_w*dt)/2.0f);
-      qhat_np1.w = t1*state_.attitude.w + t2*(-p*state_.attitude.x - q*state_.attitude.y - r*state_.attitude.z);
-      qhat_np1.x = t1*state_.attitude.x + t2*(p*state_.attitude.w + r*state_.attitude.y - q*state_.attitude.z);
-      qhat_np1.y = t1*state_.attitude.y + t2*(q*state_.attitude.w - r*state_.attitude.x + p*state_.attitude.z);
-      qhat_np1.z = t1*state_.attitude.z + t2*(r*state_.attitude.w + q*state_.attitude.x - p*state_.attitude.y);
-      state_.attitude = qhat_np1.normalize();
-    }
-    else
-    {
-      // Euler Integration
-      // (Eq. 47a Mahony Paper), but this is pretty straight-forward
-      turbomath::Quaternion qdot(0.5f * (-p*state_.attitude.x - q*state_.attitude.y - r*state_.attitude.z),
-                                 0.5f * (p*state_.attitude.w + r*state_.attitude.y - q*state_.attitude.z),
-                                 0.5f * (q*state_.attitude.w - r*state_.attitude.x + p*state_.attitude.z),
-                                 0.5f * (r*state_.attitude.w + q*state_.attitude.x - p*state_.attitude.y));
-      state_.attitude.w += qdot.w*dt;
-      state_.attitude.x += qdot.x*dt;
-      state_.attitude.y += qdot.y*dt;
-      state_.attitude.z += qdot.z*dt;
-      state_.attitude.normalize();
-    }
-  }
-
-  // Extract Euler Angles for controller
-  state_.attitude.get_RPY(&state_.roll, &state_.pitch, &state_.yaw);
-
-  // Save off adjust gyro measurements with estimated biases for control
-  state_.angular_velocity = gyro_LPF_ - bias_;
-
-  // If it has been more than 0.5 seconds since the acc update ran and we are supposed to be getting them
-  // then trigger an unhealthy estimator error
-  if (RF_.params_.get_param_int(PARAM_FILTER_USE_ACC) && now_us > 500000 + last_acc_update_us_
-      && !RF_.params_.get_param_int(PARAM_FIXED_WING))
-  {
-    RF_.state_manager_.set_error(StateManager::ERROR_UNHEALTHY_ESTIMATOR);
+    // Matrix Exponential Approximation (From Attitude Representation and Kinematic
+    // Propagation for Low-Cost UAVs by Robert T. Casey)
+    // (Eq. 12 Casey Paper)
+    // This adds 90 us on STM32F10x chips
+    float norm_w = sqrtf(sqrd_norm_w);
+    float t1 = cosf((norm_w*dt)/2.0f);
+    float t2 = 1.0f/norm_w * sinf((norm_w*dt)/2.0f);
+    quat.w = t1*quat.w + t2*(-p*quat.x - q*quat.y - r*quat.z);
+    quat.x = t1*quat.x + t2*( p*quat.w + r*quat.y - q*quat.z);
+    quat.y = t1*quat.y + t2*( q*quat.w - r*quat.x + p*quat.z);
+    quat.z = t1*quat.z + t2*( r*quat.w + q*quat.x - p*quat.y);
+    quat.normalize();
   }
   else
   {
-    RF_.state_manager_.clear_error(StateManager::ERROR_UNHEALTHY_ESTIMATOR);
+    // Euler Integration
+    // (Eq. 47a Mahony Paper)
+    turbomath::Quaternion qdot(0.5f * (-p*quat.x - q*quat.y - r*quat.z),
+                               0.5f * ( p*quat.w + r*quat.y - q*quat.z),
+                               0.5f * ( q*quat.w - r*quat.x + p*quat.z),
+                               0.5f * ( r*quat.w + q*quat.x - p*quat.y));
+    quat.w += qdot.w*dt;
+    quat.x += qdot.x*dt;
+    quat.y += qdot.y*dt;
+    quat.z += qdot.z*dt;
+    quat.normalize();
   }
 }
 
