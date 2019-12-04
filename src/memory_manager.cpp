e98b6d850ad23f78f230d920d3a2a0a695f56e45
Trey Henrichsen <treyhenrichsen@gmail.com>
2019-12-04 13:41:35 -0700

    Add memory manager

---
+++ src/memory_manager.cpp
@@ -0,0 +1,24 @@
+#include "memory_manager.h"
+#include "rosflight.h"
+
+namespace rosflight_firmware
+{
+
+MemoryManager::MemoryManager(ROSflight &rf):
+  RF_{rf}
+{
+}
+bool MemoryManager::read_memory()
+{
+  RF_.board_.memory_init();
+  ready_ = RF_.board_.memory_read(&memory_, sizeof(persistent_memory_t));
+  return ready_;
+}
+bool MemoryManager::write_memory()
+{
+  RF_.params_.prepare_write();
+  RF_.config_manager_.prepare_write();
+  return RF_.board_.memory_write(&memory_, sizeof(persistent_memory_t));
+}
+
+} // namespace rosflight_firmware
