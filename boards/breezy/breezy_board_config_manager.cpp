0b989f93e11f09cc1202a661950a5acf9302399f
Trey Henrichsen <treyhenrichsen@gmail.com>
2020-01-14 15:21:53 -0700

    Get breezy building with changes

--- boards/breezy/breezy_board_config_manager.cpp
+++ boards/breezy/breezy_board_config_manager.cpp
@@ -3,34 +3,35 @@
 
 namespace rosflight_firmware
 {
-hardware_config_t BreezyBoardConfigManager::get_max_config(device_t device)
+hardware_config_t BreezyBoardConfigManager::get_max_config(device_t device) const
 {
   (void)device;
   return 0;
 }
 
-ConfigManager::config_response BreezyBoardConfigManager::check_config_change(device_t device, hardware_config_t config)
+ConfigManager::ConfigResponse BreezyBoardConfigManager::check_config_change(device_t device, hardware_config_t config, const ConfigManager &cm) const
 {
   (void)device;
   (void)config;
-  ConfigManager::config_response response;
+  (void)cm;
+  ConfigManager::ConfigResponse response;
   response.successful = false;
   response.reboot_required = false;
   strcpy(reinterpret_cast<char*>(response.message), "Feature unsupported on naze");
   return response;
 }
 
-void BreezyBoardConfigManager::get_device_name(device_t device, uint8_t (&name)[20])
+void BreezyBoardConfigManager::get_device_name(device_t device, char (&name)[BoardConfigManager::DEVICE_NAME_LENGTH]) const
 {
   (void)device;
-  strcpy(reinterpret_cast<char*>(name), "Unsupported");
+  strcpy(name, "Unsupported");
 }
 
-void BreezyBoardConfigManager::get_config_name(device_t device, hardware_config_t config, uint8_t (&name)[20])
+void BreezyBoardConfigManager::get_config_name(device_t device, hardware_config_t config, char (&name)[BoardConfigManager::CONFIG_NAME_LENGTH]) const
 {
   (void)device;
   (void)config;
-  strcpy(reinterpret_cast<char*>(name), "Unsupported");
+  strcpy(name, "Unsupported");
 }
 
 } // namespace rosflight_firmware
