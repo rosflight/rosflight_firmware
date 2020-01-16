14e48f6949b120853aa12642dc0668d14fc2a5ca
Trey Henrichsen <treyhenrichsen@gmail.com>
2020-01-16 15:21:25 -0700

    More doxygen comments

--- include/board_config_manager.h
+++ include/board_config_manager.h
@@ -6,22 +6,27 @@
 
 namespace rosflight_firmware
 {
+/**
+ * @brief Board Config Managers handle board specific details of configurations.
+ * @details Such details include the names of devices and configurations, as well as checking
+ * The validity of configuration changes before they take effect.
+ */
 class BoardConfigManager
 {
 public:
 
   /**
    * @brief Get the largest number that is valid for the configuration of a given device
-   * This number is inclusive, i.e. a value of 2 means 0, 1, or 2 are valid
+   * @details This number is inclusive, i.e. a value of 2 means 0, 1, or 2 are valid
    * For devices that are not configurable, returns 0
    * @param device Any device
    */
   virtual hardware_config_t get_max_config(device_t device) const = 0;
   /**
    * @brief Check if a config change is allowed.
-   * If the response indicates success, then the config manager accepts the change
-   * If not, the config manager returns the error indicated
-   * Implementations should not assume that either the device or the config are valid
+   * @details If the response indicates success, then the config manager accepts the change.
+   * If not, the config manager returns the error indicated.
+   * Implementations should not assume that either the device or the config are valid.
    * @param device The device whose configuration is being changed
    * @param config The new configuration for the device
    * @param cm The ConfigManager with the current configurations
@@ -30,20 +35,15 @@ public:
   static constexpr int DEVICE_NAME_LENGTH = 20;
   static constexpr int CONFIG_NAME_LENGTH = 20;
   /**
-   * Do not assume that the device or config are valid numbers
-   * When passed an invalid argument, it is better to return a string so indicating than an
-   * empty string
-   */
-  /**
    * @brief Returns the name of the device
-   * Do not assume that the device number is valid.
+   * @details Do not assume that the device number is valid.
    * When passed an invalid device, it is better to return a string so indicating than an empty string
    * @param device Any device
    */
   virtual void get_device_name(device_t device, char (&name)[DEVICE_NAME_LENGTH]) const=0;
   /**
    * @brief Returns the name of a configuration
-   * Do not assume that the device number or configuration are valid.
+   * @details Do not assume that the device number or configuration are valid.
    * When passed an invalid device or configuration, it is better to return a string so indicating
    * than an empty string.
    * @param device Any device
