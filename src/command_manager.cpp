1dc696ebfc0e0c66070bf35dc770f3b84439d06f
Trey Henrichsen <treyhenrichsen@gmail.com>
2020-04-29 16:37:29 -0600

    Fix style

--- src/command_manager.cpp
+++ src/command_manager.cpp
@@ -84,7 +84,8 @@ void CommandManager::init_failsafe()
 {
   float failsafe_thr_param = RF_.params_.get_param_float(PARAM_FAILSAFE_THROTTLE);
   bool fixedwing = RF_.params_.get_param_int(PARAM_FIXED_WING);
-  if (!fixedwing && (failsafe_thr_param < 0. || failsafe_thr_param > 1.0)) // fixed wings always have 0 failsafe throttle
+  if (!fixedwing
+      && (failsafe_thr_param < 0. || failsafe_thr_param > 1.0)) // fixed wings always have 0 failsafe throttle
   {
     RF_.state_manager_.set_error(StateManager::ERROR_INVALID_FAILSAFE);
     failsafe_thr_param = 0.;
