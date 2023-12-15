0f4d0a9c71b66f28b68bfec241c14f3aefc05e6a
Brandon Sutherland <brandonsutherland2@gmail.com>
2023-12-15 10:37:56 -0700

    Updated linter and .clang-format for ROS2

--- scripts/fix-code-style.sh
+++ scripts/fix-code-style.sh
@@ -6,6 +6,6 @@ SCRIPTPATH=`dirname $SCRIPT`
 echo #SCRIPTPATH
 cd $SCRIPTPATH/..
 
-find . -iname "*.h" -o -iname "*.cpp" | \
-egrep -v "^(./comms/mavlink/v1.0/|./boards/airbourne/airbourne/|./boards/breezy/breezystm32|./.git|./test/build)" | \
+find . -iname "*.h" -o -iname "*.hpp" -o -iname "*.cpp" | \
+grep -Ev "^(./comms/mavlink/v1.0/|./boards/airbourne/airbourne/|./boards/breezy/breezystm32|./.git|./test/build)" | \
 xargs clang-format -i
