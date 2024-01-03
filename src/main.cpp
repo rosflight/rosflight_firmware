8be7e65b96c48aaba65f837893307569ffb2bdb9
Brandon Sutherland <brandonsutherland2@gmail.com>
2024-01-03 15:28:44 -0700

    Moved main.cpp from boards to firmware src

---
+++ src/main.cpp
@@ -0,0 +1,70 @@
+/**
+ ******************************************************************************
+ * File     : main.cpp
+ * Date     : Oct 5, 2023
+ ******************************************************************************
+ *
+ * Copyright (c) 2023, AeroVironment, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1.Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2.Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3.Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************
+ **/
+
+#include <Varmint.h>
+#include <mavlink.h>
+#include <rosflight.h>
+
+extern Varmint varmint;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int main(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @fn int main(void)
+ * @brief Program Start
+ */
+int main(void)
+{
+  // Rosflight board code
+  varmint.init_board();
+
+  // Rosflight base code
+  rosflight_firmware::Mavlink mavlink(varmint);
+  rosflight_firmware::ROSflight firmware(varmint, mavlink);
+
+  firmware.init();
+
+  while (true) { firmware.run(); }
+}
