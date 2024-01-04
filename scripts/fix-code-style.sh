7403f1dbb2377d7c99eaf09cd30a9dc8c4e2f608
Brandon Sutherland <brandonsutherland2@gmail.com>
2024-01-03 17:58:21 -0700

    Misc change to code style script

--- scripts/fix-code-style.sh
+++ scripts/fix-code-style.sh
@@ -7,5 +7,5 @@ echo #SCRIPTPATH
 cd $SCRIPTPATH/..
 
 find . -iname "*.h" -o -iname "*.hpp" -o -iname "*.cpp" | \
-    grep -Ev "^(./comms/mavlink/v1.0/|./.git|./boards/varmint/lib/)" | \
+    grep -Ev "^(./comms/mavlink/v1.0|./.git|./boards)" | \
 xargs clang-format -i
