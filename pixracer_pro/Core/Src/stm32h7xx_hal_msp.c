680564c43ce8c37fc81267fa9689aa99f8eb26a8
Phil Tokumaru <tokumaru@avinc.com>
2024-07-08 09:26:47 -0700

    Misc Project Cleanup

--- pixracer_pro/Core/Src/stm32h7xx_hal_msp.c
+++ pixracer_pro/Core/Src/stm32h7xx_hal_msp.c
@@ -1864,7 +1864,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     __HAL_LINKDMA(huart,hdmatx,hdma_usart2_tx);
 
     /* USART2 interrupt Init */
-    HAL_NVIC_SetPriority(USART2_IRQn, 1, 0);
+    HAL_NVIC_SetPriority(USART2_IRQn, 0, 0);
     HAL_NVIC_EnableIRQ(USART2_IRQn);
   /* USER CODE BEGIN USART2_MspInit 1 */
 
