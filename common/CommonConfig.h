3c3158b28fc7a8fac1ac0a223806fb9f39f3dab6
Phil Tokumaru <tokumaru@avinc.com>
2024-07-15 13:08:33 -0700

    Added CommonConfig.h file

---
+++ common/CommonConfig.h
@@ -0,0 +1,176 @@
+/**
+ ******************************************************************************
+ * File     : CommonConfig.h
+ * Date     : Sep 23, 2023
+ ******************************************************************************
+ *
+ * Copyright (c) 2023, AeroVironment, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1.Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2.Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3.Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************
+ **/
+
+#include "stm32h7xx_hal.h"
+
+#include <main.h>
+
+// Buffers types
+
+#define MY_FAST_BUFFER __attribute__((section("my_buffers"))) __attribute__((aligned(32))) static
+#define MY_BDMA_BUFFER                                                                             \
+  __attribute__((section("my_bdma_buffers"))) __attribute__((aligned(32))) static
+#define MY_DMA_BUFFER __attribute__((section("my_dma_buffers"))) __attribute__((aligned(32))) static
+#define INITIALIZE_DATA_BUFFER __attribute__((section(".data"))) __attribute__((aligned(32))) static
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// MiddleWare name : AL94.I-CUBE-USBD-COMPOSITE.1.0.3
+//
+#define VCP_Transmit(buffer, length) CDC_Transmit(0, buffer, length)
+//#define _USBD_USE_HS false
+//extern PCD_HandleTypeDef hpcd_USB_OTG_FS; // USB FS (48 MB/s)
+
+#define _USBD_USE_CDC_ACM true // /dev/ttyACM* device type
+#define _USBD_CDC_ACM_COUNT 1
+#define _USBD_USE_CDC_RNDIS false
+#define _USBD_USE_CDC_ECM false
+#define _USBD_USE_HID_MOUSE false
+#define _USBD_USE_HID_KEYBOARD false
+#define _USBD_USE_HID_CUSTOM false
+#define _USBD_USE_UAC_MIC false
+#define _USBD_USE_UAC_SPKR false
+#define _USBD_USE_UVC false
+#define _USBD_USE_MSC false
+#define _USBD_USE_DFU false
+#define _USBD_USE_PRNTR false
+#define _STM32F1_DEVICE false // F1 devices are a special case
+//
+// End MiddleWare name : AL94.I-CUBE-USBD-COMPOSITE.1.0.3
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#define EPOCH_HZ (400)
+#define EPOCH_US (1000000 / EPOCH_HZ)
+
+#define FIFO_MIN_BUFFERS 2 // Defaults to double buffered with really slow data.
+
+#define I2C_DMA_MAX_BUFFER_SIZE 64
+#define SPI_DMA_MAX_BUFFER_SIZE 64
+
+// SDMMC
+#define SD_HSD (&hsd1)
+#define SD_HSD_INSTANCE (SDMMC1)
+
+#define DRIVER_OK (0x00000000)
+#define DRIVER_ID_MISMATCH (0x00000002)
+#define DRIVER_SELF_DIAG_ERROR (0x00000004)
+#define DRIVER_HAL_ERROR (0x00000008)
+#define DRIVER_HAL_ERROR2 (0x00000010)
+#define DRIVER_FIFO_INIT_ERROR (0x00000020)
+#define UBX_ACK (0x00000040)
+#define UBX_NAK (0x00000080)
+#define UBX_ACKNAK_FAIL (0x00000100)
+#define UBX_SUCCESS (0x00000200)
+#define UBX_FAIL_BAUD_CHANGE (0x00000400)
+#define VOLTAGE_SET_FAIL (0x00000800)
+#define TIMERS_INVALID (0x00001000)
+
+extern ADC_HandleTypeDef hadc1;
+extern ADC_HandleTypeDef hadc2;
+extern ADC_HandleTypeDef hadc3;
+extern DMA_HandleTypeDef hdma_adc1;
+extern DMA_HandleTypeDef hdma_adc2;
+extern DMA_HandleTypeDef hdma_adc3;
+
+extern CRC_HandleTypeDef hcrc;
+
+extern FDCAN_HandleTypeDef hfdcan1;
+
+extern I2C_HandleTypeDef hi2c1;
+extern I2C_HandleTypeDef hi2c2;
+extern DMA_HandleTypeDef hdma_i2c1_rx;
+extern DMA_HandleTypeDef hdma_i2c1_tx;
+
+extern RNG_HandleTypeDef hrng;
+
+extern RTC_HandleTypeDef hrtc;
+
+extern SD_HandleTypeDef hsd1;
+
+extern SPI_HandleTypeDef hspi1;
+extern SPI_HandleTypeDef hspi2;
+extern SPI_HandleTypeDef hspi3;
+extern SPI_HandleTypeDef hspi4;
+extern SPI_HandleTypeDef hspi5;
+extern SPI_HandleTypeDef hspi6;
+
+extern DMA_HandleTypeDef hdma_spi1_rx;
+extern DMA_HandleTypeDef hdma_spi1_tx;
+extern DMA_HandleTypeDef hdma_spi2_rx;
+extern DMA_HandleTypeDef hdma_spi2_tx;
+extern DMA_HandleTypeDef hdma_spi3_rx;
+extern DMA_HandleTypeDef hdma_spi3_tx;
+extern DMA_HandleTypeDef hdma_spi4_rx;
+extern DMA_HandleTypeDef hdma_spi4_tx;
+extern DMA_HandleTypeDef hdma_spi5_rx;
+extern DMA_HandleTypeDef hdma_spi5_tx;
+extern DMA_HandleTypeDef hdma_spi6_rx;
+extern DMA_HandleTypeDef hdma_spi6_tx;
+
+extern TIM_HandleTypeDef htim1;
+extern TIM_HandleTypeDef htim3;
+extern TIM_HandleTypeDef htim4;
+extern TIM_HandleTypeDef htim5;
+extern TIM_HandleTypeDef htim7;
+extern TIM_HandleTypeDef htim8;
+extern TIM_HandleTypeDef htim12;
+extern TIM_HandleTypeDef htim15;
+
+extern UART_HandleTypeDef huart1;
+extern UART_HandleTypeDef huart2;
+extern UART_HandleTypeDef huart3;
+extern UART_HandleTypeDef huart4;
+extern UART_HandleTypeDef huart5;
+extern UART_HandleTypeDef huart6;
+extern UART_HandleTypeDef huart7;
+
+extern DMA_HandleTypeDef hdma_usart1_rx;
+extern DMA_HandleTypeDef hdma_usart1_tx;
+extern DMA_HandleTypeDef hdma_usart2_rx;
+extern DMA_HandleTypeDef hdma_usart2_tx;
+extern DMA_HandleTypeDef hdma_usart3_rx;
+extern DMA_HandleTypeDef hdma_usart3_tx;
+extern DMA_HandleTypeDef hdma_usart6_rx;
+extern DMA_HandleTypeDef hdma_usart6_tx;
+
+extern DMA_HandleTypeDef hdma_uart4_rx;
+extern DMA_HandleTypeDef hdma_uart4_tx;
+extern DMA_HandleTypeDef hdma_uart5_rx;
+extern DMA_HandleTypeDef hdma_uart5_tx;
+extern DMA_HandleTypeDef hdma_uart7_rx;
+extern DMA_HandleTypeDef hdma_uart7_tx;
+extern DMA_HandleTypeDef hdma_uart8_rx;
+extern DMA_HandleTypeDef hdma_uart8_tx;
