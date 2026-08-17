277c8ab637675350a23c2408f7fd199833412736
Mat Haskell <mhaskell9@gmail.com>
2026-08-17 15:57:59 -0600

    refactor(cmake): clean up board configuration

--- boards/varmint_h7/common/stm32_h7_board.cmake
+++ boards/varmint_h7/common/stm32_h7_board.cmake
@@ -4,10 +4,22 @@ function(add_stm32_h7_board board_name board_chip)
   # be passed into the function.
   set(board_source_dir "${CMAKE_CURRENT_SOURCE_DIR}")
   set(common_source_dir "${CMAKE_CURRENT_FUNCTION_LIST_DIR}")
+  set(supported_board_chips H753 H743)
+
+  ###############
+  ## Prechecks ##
+  ###############
 
   if(NOT TARGET rosflight_firmware::firmware_main)
     message(FATAL_ERROR "add_stm32_h7_board() "
       "requires rosflight_firmware::firmware_main to be defined first"
+    )
+  endif()
+
+  if(NOT board_chip IN_LIST supported_board_chips)
+    list(JOIN supported_board_chips ", " supported_board_chips_message)
+    message(FATAL_ERROR "Unknown board_chip: ${board_chip}.\n"
+      "Valid options: ${supported_board_chips_message}"
     )
   endif()
 
@@ -32,13 +44,9 @@ function(add_stm32_h7_board board_name board_chip)
     set(startup_file
       "${common_source_dir}/Core/Startup/startup_stm32h753vihx.s"
     )
-  elseif(board_chip STREQUAL "H743")
+  else()
     set(startup_file
       "${common_source_dir}/Core/Startup/startup_stm32h743iikx.s"
-    )
-  else()
-    message(FATAL_ERROR
-      "Unknown board_chip: ${board_chip}. Must be H753 or H743"
     )
   endif()
 
@@ -54,42 +62,42 @@ function(add_stm32_h7_board board_name board_chip)
   list(FILTER board_sources EXCLUDE REGEX ".*/syscalls\\.c$")
   list(FILTER board_sources EXCLUDE REGEX ".*/sysmem\\.c$")
 
-  set(all_sources
-    ${common_hal_sources}
-    ${common_system_sources}
-    ${startup_file}
-    ${board_sources}
-  )
-
   #####################
   ## Main Executable ##
   #####################
 
-  ### build target ###
-  add_executable(${board_name}.elf ${all_sources})
-  target_compile_definitions(${board_name}.elf PRIVATE
+  set(target_name "${board_name}.elf")
+
+  add_executable("${target_name}")
+  target_sources("${target_name}" PRIVATE
+    ${common_hal_sources}
+    ${common_system_sources}
+    "${startup_file}"
+    ${board_sources}
+  )
+  target_compile_definitions("${target_name}" PRIVATE
     DEBUG
     USE_HAL_DRIVER
-    STM32${board_chip}xx
+    "STM32${board_chip}xx"
   )
-  target_compile_features(${board_name}.elf PRIVATE c_std_11 cxx_std_17)
-  target_include_directories(${board_name}.elf PRIVATE
-    ${board_source_dir}/Core/Inc
-    ${board_source_dir}/specific
-    ${common_source_dir}
-    ${common_source_dir}/stm32_drivers/STM32H7xx_HAL_Driver/Inc
-    ${common_source_dir}/stm32_drivers/STM32H7xx_HAL_Driver/Inc/Legacy
-    ${common_source_dir}/stm32_drivers/CMSIS/Device/ST/STM32H7xx/Include
-    ${common_source_dir}/stm32_drivers/CMSIS/Include
-    ${common_source_dir}/sensor_drivers
-    ${common_source_dir}/AL94_USB_Composite
+  target_compile_features("${target_name}" PRIVATE c_std_11 cxx_std_17)
+  target_include_directories("${target_name}" PRIVATE
+    "${board_source_dir}/Core/Inc"
+    "${board_source_dir}/specific"
+    "${common_source_dir}"
+    "${common_source_dir}/stm32_drivers/STM32H7xx_HAL_Driver/Inc"
+    "${common_source_dir}/stm32_drivers/STM32H7xx_HAL_Driver/Inc/Legacy"
+    "${common_source_dir}/stm32_drivers/CMSIS/Device/ST/STM32H7xx/Include"
+    "${common_source_dir}/stm32_drivers/CMSIS/Include"
+    "${common_source_dir}/sensor_drivers"
+    "${common_source_dir}/AL94_USB_Composite"
   )
-  target_link_libraries(${board_name}.elf PRIVATE
+  target_link_libraries("${target_name}" PRIVATE
     rosflight_firmware::firmware_main
   )
 
   set(linker_script "${common_source_dir}/STM32H7LinkerScript.ld")
-  target_link_options(${board_name}.elf PRIVATE
+  target_link_options("${target_name}" PRIVATE
     "-T${linker_script}"
     "-Wl,-Map=${CMAKE_CURRENT_BINARY_DIR}/${board_name}.elf.map,--cref"
   )
@@ -97,11 +105,21 @@ function(add_stm32_h7_board board_name board_chip)
   set(hex_file "${CMAKE_CURRENT_BINARY_DIR}/${board_name}.hex")
   set(bin_file "${CMAKE_CURRENT_BINARY_DIR}/${board_name}.bin")
 
-  add_custom_command(TARGET ${board_name}.elf POST_BUILD
-    COMMAND ${CMAKE_OBJCOPY} -Oihex $<TARGET_FILE:${board_name}.elf> ${hex_file}
+  add_custom_command(TARGET "${target_name}" POST_BUILD
+    BYPRODUCTS
+      "${hex_file}"
+      "${bin_file}"
     COMMAND
-      ${CMAKE_OBJCOPY} -Obinary $<TARGET_FILE:${board_name}.elf> ${bin_file}
+      "${CMAKE_OBJCOPY}"
+      -O ihex
+      "$<TARGET_FILE:${target_name}>"
+      "${hex_file}"
+    COMMAND
+      "${CMAKE_OBJCOPY}"
+      -O binary
+      "$<TARGET_FILE:${target_name}>"
+      "${bin_file}"
     COMMENT "Building Artifacts: ${hex_file} and ${bin_file}"
+    VERBATIM
   )
 endfunction()
-
